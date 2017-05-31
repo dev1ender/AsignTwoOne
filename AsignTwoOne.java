e285ae40a4fd6815e159babf5d04a352fff70e84
ITSOK\anonm <dev1ender@gmail.com>
2017-05-31 21:37:11 +0530

    final commmit

---
+++ AsignTwoOne.java
@@ -0,0 +1,19 @@
+public class AsignTwoOne {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		for (int i =1 ;i<=5; i++)
+		{
+			for(int j=1 ; j<=5 ;j++)
+			{
+				if (j==i || j == 5-i+1)
+				System.out.print("*");
+				else
+					System.out.print("_");
+			}
+			System.out.println("");
+		}
+	}
+
+}
